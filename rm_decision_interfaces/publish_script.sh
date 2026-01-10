bafd0821597af5d3195c4c84983b92b85eb33c54
root <root@Ark.localdomain>
2026-01-10 22:49:38 +0800

    Add basic behavior tree strategy

--- rm_decision_interfaces/publish_script.sh
+++ rm_decision_interfaces/publish_script.sh
@@ -8,7 +8,7 @@ ros2 topic pub -r 10 /decision_num rm_decision_interfaces/msg/DecisionNum "{
 
 ros2 topic pub -r 10 /robot_status rm_decision_interfaces/msg/RobotStatus "{
     robot_id: 7,
-    current_hp: 600,
+    current_hp: 100,
     shooter_heat: 0,
     team_color: 0,
     is_attacked: 0,
