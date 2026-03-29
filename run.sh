4a50268c5025e80ae253a1af16129466e53ea298
Bourne-z04 <your-email@example.com>
2026-03-29 18:12:52 +0800

    v1策略

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 source ./install/setup.bash
 
 # Launch the behavior tree
-ros2 launch rm_behavior_tree rm_behavior_tree.launch.py style:=basic.xml use_sim_time:=True
+ros2 launch rm_behavior_tree rm_behavior_tree.launch.py style:=v1.xml use_sim_time:=True
