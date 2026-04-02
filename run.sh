8139564ffb88e6b44e7d0df8cce6c36bf56918fa
Bourne-z04 <your-email@example.com>
2026-04-03 01:43:31 +0800

    v0v1v2 0143

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 source ./install/setup.bash
 
 # Launch the behavior tree
-ros2 launch rm_behavior_tree rm_behavior_tree.launch.py style:=v1.xml use_sim_time:=False
+ros2 launch rm_behavior_tree rm_behavior_tree.launch.py style:=v2.xml use_sim_time:=True
